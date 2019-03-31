e13d33da596b72c7d1f6f917e2ea724189038d37
Mutturaj Hulagabal <hulagabal@gmail.com>
2019-03-31 22:02:18 +0530

    updated element

--- muttu11/src/test/java/muttu11/LoginTest.java
+++ muttu11/src/test/java/muttu11/LoginTest.java
@@ -1,6 +1,10 @@
 package muttu11;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import pages.LoginPage;
 
 public class LoginTest extends BaseClass {
 
@@ -8,6 +12,17 @@ public class LoginTest extends BaseClass {
 	public void test1() {
 		
 		System.out.println(driver.getTitle());
+		
+		LoginPage loginPage=PageFactory.initElements(driver, LoginPage.class);
+		
+		loginPage.login();
+		
+		Assert.assertEquals(driver.getTitle(), "Facebook");
+		
+		loginPage.logout(driver);
+	
+		
+		
 	}
 
 }
