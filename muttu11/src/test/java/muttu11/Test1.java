bcf1eeca2106da26b1e5f0b3db36d6b79f15feeb
Mutturaj Hulagabal <hulagabal@gmail.com>
2019-03-31 17:11:48 +0530

    pom

--- muttu11/src/test/java/muttu11/Test1.java
+++ muttu11/src/test/java/muttu11/Test1.java
@@ -1,38 +1,13 @@
 package muttu11;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Test1 {
-	private WebDriver driver;
-
-	@BeforeClass
-	public void check() {
-
-		driver = new ChromeDriver();
-
-		driver.manage().deleteAllCookies();
-
-		driver.manage().window().maximize();
-
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
-	}
+public class Test1 extends BaseClass {
 
 	@Test
-	public void getBrowser() {
+	public void test1() {
 
-		driver.get("https://github.com/bonigarcia/webdrivermanager");
-	}
-
-	@AfterClass
-	public void tearDown() {
-		driver.close();
+		System.out.println(driver.getTitle());
 	}
 
 }
